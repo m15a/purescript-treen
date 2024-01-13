164ff9189ecd72b20259cf7291db8749bb7dc943
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-13 11:52:36 +0900

    chore: add Purescript toolchain

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Show any lines as tree-like format.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+    purescript-overlay = {
+      url = "github:thomashoneyman/purescript-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, purescript-overlay }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        purescript-overlay.overlays.default
+      ];
+    };
+  in
+  {
+    devShells.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        esbuild
+        nodejs
+        purs
+        purs-backend-es
+        spago-unstable
+        purs-tidy
+      ];
+    };
+  });
+}
