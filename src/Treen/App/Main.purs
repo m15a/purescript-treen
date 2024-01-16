a9592f22d291861c5b4a3eb58a60d38a27001cc2
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-16 14:31:08 +0900

    feat: Implement basic app as a command-line pipe

---
+++ src/Treen/App/Main.purs
@@ -0,0 +1,22 @@
+module Treen.App.Main (main) where
+
+import Prelude
+import Data.Foldable (for_)
+import Data.List (fromFoldable, mapMaybe) as L
+import Data.String.Pattern (Pattern(..))
+import Data.String.Util (lines)
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Node.Process (stdin, stdout)
+import Node.Stream.Aff (fromStringUTF8, toStringUTF8, readAll, write)
+import Treen.Data.Clade (bundle, printClade)
+import Treen.Data.Lineage (fromString)
+
+main :: Effect Unit
+main = launchAff_ do
+  ss <- readAll stdin >>= toStringUTF8 <#> lines <#> L.fromFoldable
+  let clades = bundle $ L.mapMaybe (fromString $ Pattern "/") ss
+  for_ clades \c -> do
+    write stdout =<< fromStringUTF8 (printClade c <> "\n")
+
+-- vim: ft=haskell tw=88
