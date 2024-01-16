590df25358228415aeab1f6325bd5ce2b78103a6
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-16 16:40:28 +0900

    chore: tidy test codes

--- test/Test/Data/String/Util/Spec.purs
+++ test/Test/Data/String/Util/Spec.purs
@@ -12,28 +12,31 @@ stringUtilSpec = describe "Data.String.Util" do
   describe "trimMargin" do
 
     it "removes the first empty line" do
-      let actual = trimMargin
-            """  
-            
-            
-            a"""
-          expected = "\n\na"
+      let
+        actual = trimMargin
+          """  
+          
+          
+          a"""
+        expected = "\n\na"
       actual `shouldEqual` expected
 
     it "removes each line's margin" do
-      let actual = trimMargin
-            """
-             a
-            b
-               c"""
-          expected = " a\nb\n   c"
+      let
+        actual = trimMargin
+          """
+           a
+          b
+             c"""
+        expected = " a\nb\n   c"
       actual `shouldEqual` expected
 
     it "does not remove the last newline" do
-      let actual = trimMargin
-            """
-            a
-            
-            """
-          expected = "a\n\n"
+      let
+        actual = trimMargin
+          """
+          a
+          
+          """
+        expected = "a\n\n"
       actual `shouldEqual` expected
